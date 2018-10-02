26f65c4503c861a9c0b2c16f1884a4e1e68e91bc
DuniwayRobotics <nmurray@msd.k12.or.us>
2018-10-02 15:36:58 -0700

    Added extends and @Autonomous declaration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AftershockAutonomous2018.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AftershockAutonomous2018.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AftershockAutonomous2018 {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(group = "Aftershock", name = "Aftershock Autonomous 2018")
+public class AftershockAutonomous2018 extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }

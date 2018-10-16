be12fbf8635c8dd159a343bac66eaa441f5f907f
DuniwayRobotics <nmurray@msd.k12.or.us>
2018-10-16 16:20:35 -0700

    Implementing the range sensor and adding the MRI color beacon.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MRI_Range.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MRI_Range.java
@@ -14,6 +14,7 @@ Support is available by emailing support@modernroboticsinc.com.
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -21,32 +22,45 @@ import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp(name = "Range", group = "MRI")
 public class MRI_Range extends OpMode {
 
-    byte[] range1Cache; //The read will return an array of bytes. They are stored in this variable
+    TestbotHardware robot   = new TestbotHardware();   // Use a Pushbot's hardware
 
-    I2cAddr RANGE1ADDRESS = new I2cAddr(0x14); //Default I2C address for MR Range (7-bit)
-    public static final int RANGE1_REG_START = 0x04; //Register to start reading
-    public static final int RANGE1_READ_LENGTH = 2; //Number of byte to read
+    byte red = 0;
+    byte green = 0;
+    byte blue = 0;
 
-    public I2cDevice RANGE1;
-    public I2cDeviceSynch RANGE1Reader;
+    int colorNumber;
 
     @Override
     public void init() {
-        RANGE1 = hardwareMap.i2cDevice.get("range");
-        RANGE1Reader = new I2cDeviceSynchImpl(RANGE1, RANGE1ADDRESS, false);
-        RANGE1Reader.engage();
+        robot.init(hardwareMap);
     }
 
     @Override
     public void loop() {
-        range1Cache = RANGE1Reader.read(RANGE1_REG_START, RANGE1_READ_LENGTH);
+        double distance = robot.rangeSensor.getDistance(DistanceUnit.CM);
 
-        telemetry.addData("Ultra Sonic", range1Cache[0] & 0xFF);
-        telemetry.addData("ODS", range1Cache[1] & 0xFF);
+        if (distance > 10) {
+            robot.dc_2.setPower(1);
+        } else {
+            robot.dc_2.setPower(0);
+        }
+
+        if (robot.touch.isPressed()) {
+            robot.beacon.green();
+        } else {
+            robot.beacon.red();
+        }
+
+        telemetry.addData("distance:", distance);
+        telemetry.update();
     }
 
     @Override
